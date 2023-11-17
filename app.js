6f038b09e5f65ae7c12ab70775d2c8429d5144d7
Kyle Seto <kyle@MacBook-Pro.fios-router.home>
2023-11-17 15:42:27 -0500

    app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import Hello from './hello.js';
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from './modules/routes.js';
+import "dotenv/config";
 const app = express();
 app.use(cors());
 app.use(express.json());
